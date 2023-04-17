c5a591d8af4d27e29db447a6bd4bbc1c1784ad81
Crowdbotics <engineering@crowdbotics.com>
2023-04-17 00:10:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/throbbing_dew_dev_76940/MainActivity.java
@@ -0,0 +1,15 @@
+package com.throbbing_dew_dev_76940;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "throbbing_dew_dev_76940";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-dew-dev-76940",
+  url: "https://throbbing-dew-dev-76940.botics.co",
+  api: "https://throbbing-dew-dev-76940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
